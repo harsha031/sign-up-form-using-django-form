c12d2ad5508a9ba9c47e46224cfdb97be2b62d5f
harsha031 <92251457+harsha031@users.noreply.github.com>
2022-07-16 15:03:48 +0530

    Add files via upload

---
+++ crispyproj/core/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .forms import BasicForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = BasicForm(request.POST)
+        if form.is_valid():
+            pass
+
+    else:
+        form = BasicForm()
+    return render(request, 'signup.html', {'form': form})
+
+def crispy_signup(request):
+    if request.method == 'POST':
+        form = BasicForm(request.POST)
+        if form.is_valid():
+            pass
+    else:
+        form = BasicForm()
+    return render(request, 'crispysignup.html', {'form': form})
